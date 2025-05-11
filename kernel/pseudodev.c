f246557f34b4bc4eaa457192cd845d3ba9cc0f77
FedorKhokhryakov <fekhokh@gmail.com>
2025-05-11 16:24:08 +0300

    fix

--- kernel/pseudodev.c
+++ kernel/pseudodev.c
@@ -14,12 +14,16 @@
 #define PSEUDO_URANDOM    2
 #define PSEUDO_NULLSTAT   3
 
+#define ZERO_BUF_SIZE 128
+static char zerobuf[ZERO_BUF_SIZE] = {0};
+
 struct nullstat_counter {
     uint64 total_bytes;
     struct spinlock lock;
 } ns_counter;
 
 uint64 pseudo_rand_seed = 1;
+struct spinlock rand_lock;
 
 static uint64 lcg(uint seed) {
     return (515487153 * seed + 981641ULL) % 2286661337ULL;
@@ -32,17 +36,23 @@ int pseudodev_read(short minor, int user_dst, uint64 dst, int len) {
             return 0;
 
         case PSEUDO_ZERO:
-            byte = 0;
-            for (int i = 0; i < len; ++i) {
-                if (either_copyout(user_dst, dst + i, &byte, 1) < 0)
+            int remaining = len;
+            uint64 off = 0;
+            while (remaining > 0) {
+                int chunk = remaining > ZERO_BUF_SIZE ? ZERO_BUF_SIZE : remaining;
+                if (either_copyout(user_dst, dst + off, zerobuf, chunk) < 0)
                     return -1;
+                off += chunk;
+                remaining -= chunk;
             }
             return len;
 
         case PSEUDO_URANDOM:
             for (int i = 0; i < len; ++i) {
+                acquire(&rand_lock);
                 pseudo_rand_seed = lcg(pseudo_rand_seed);
                 byte = pseudo_rand_seed & 0xFF;
+                release(&rand_lock);
                 if (either_copyout(user_dst, dst + i, &byte, 1) < 0)
                     return -1;
             }
@@ -77,7 +87,9 @@ int pseudodev_write(short minor, int user_src, uint64 src, int len) {
             uint64 new_seed;
             if (either_copyin(&new_seed, user_src, src, len) < 0)
                 return -1;
+            acquire(&rand_lock);
             pseudo_rand_seed = new_seed ? new_seed : 1;
+            release(&rand_lock);
             return len;
 
         case PSEUDO_NULLSTAT:
@@ -92,6 +104,7 @@ int pseudodev_write(short minor, int user_src, uint64 src, int len) {
 }
 
 void pseudodev_init(void) {
+    initlock(&rand_lock, "randseed");
     initlock(&ns_counter.lock, "nullstat");
     devsw[DEV].read = pseudodev_read;
     devsw[DEV].write = pseudodev_write;
